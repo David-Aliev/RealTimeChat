6caf4e53edbd78731f67ff82901de4d13019edac
david <davidaliev664@gmail.com>
2024-12-17 12:24:21 +0200

    feat: (backend) added user scheme && connection to mongo && signup controller

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,21 @@ import express from "express";
 import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.routes.js";
-const app = express();
+import connectToMongoDb from "./db/connectToMongoDB.js";
 
-dotenv.config();
+const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send("hello world");
-});
+dotenv.config();
 
+// app.get("/", (req, res) => {
+//   res.send("hello world");
+// });
+
+app.use(express.json());
 app.use("/api/auth", authRoutes);
 
-app.listen(PORT, () => console.log(`server is running on PORT ${PORT}`));
+app.listen(PORT, () => {
+  connectToMongoDb();
+  console.log(`server is running on PORT ${PORT}`);
+});

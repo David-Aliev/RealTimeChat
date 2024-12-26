121e756776027b4e883cb668d88dce7ca794f704
David-Aliev <davidaliev664@gmail.com>
2024-12-26 16:23:42 +0200

    feat: add endpoint to get all users && getMessages between two users

--- backend/routes/message.routes.js
+++ backend/routes/message.routes.js
@@ -1,8 +1,11 @@
 import express from "express";
 import { sendMessage } from "../controllers/message.controller.js";
 import protectRoute from "../middleware/protectRoute.js";
+import { getMessages } from "../controllers/message.controller.js";
 const router = express.Router();
 
 router.post("/send/:id", protectRoute, sendMessage);
+router.get("/:id", protectRoute, getMessages);
 
 export default router;
+// {"_id":{"$oid":"676146ca5d999cefcc1c2e8b"},"fullName":"ivan","username":"zolo","password":"1337228","gender":"male","profilePic":"https://avatar.iran.liara.run/public/boy?username=zolo","__v":{"$numberInt":"0"}}
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
 import connectToMongoDb from "./db/connectToMongoDB.js";
 
 const app = express();
@@ -16,8 +19,10 @@ dotenv.config();
 
 app.use(express.json());
 app.use(cookieParser());
+
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
   connectToMongoDb();
